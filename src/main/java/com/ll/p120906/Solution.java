4151bd1acb886756e0c77968113ad8443aa2458b
tkdtn4657 <tkdtn101226@gmail.com>
2023-03-12 17:21:16 +0900

    Test t2 BLUE(리팩토링할게 없어서 그냥 줄만바꿈)

--- src/main/java/com/ll/p120906/Solution.java
+++ src/main/java/com/ll/p120906/Solution.java
@@ -4,12 +4,9 @@ public class Solution {
     public int solution(int n) {
         String s = Integer.toString(n);
         int sum = 0;
-
         for(int i = 0; i < s.length(); i++){
             sum += Character.getNumericValue(s.charAt(i));
         }
-
-
         return sum;
     }
 }
