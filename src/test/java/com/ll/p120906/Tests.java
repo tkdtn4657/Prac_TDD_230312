4c81f969bca4edf7ffadffc7d87c9623a299d271
tkdtn4657 <tkdtn101226@gmail.com>
2023-03-12 17:01:38 +0900

    Test t1 RED

--- src/test/java/com/ll/p120906/Tests.java
+++ src/test/java/com/ll/p120906/Tests.java
@@ -1,6 +1,16 @@
 package com.ll.p120906;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 public class Tests {
 
-
+    @Test
+    @DisplayName("첫 테스트 Input : 1234 / Output : 10 ")
+    void t1(){
+        assertThat( new Solution().solution(1234)).isEqualTo(10);
+    }
 }
