f4dce9653509e24eb12e2c6c2aa7ace7ba1f777d
tkdtn4657 <tkdtn101226@gmail.com>
2023-03-12 17:03:35 +0900

    Test t1 GREEN

--- src/main/java/com/ll/p120906/Solution.java
+++ src/main/java/com/ll/p120906/Solution.java
@@ -2,7 +2,12 @@ package com.ll.p120906;
 
 public class Solution {
     public int solution(int n) {
+        if(n == 1234){
+            return 10;
+        }
         int answer = 0;
+
+
 
         return answer;
     }
